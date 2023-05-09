41edf7bc5fce83373e8c3358e3b783672bff16fa
Boy Sugijakko <53815726+boysugi20@users.noreply.github.com>
2023-05-09 12:48:21 +0700

    :zap: changed icons

--- lib/styles/color.dart
+++ lib/styles/color.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 // usage: Container(color: AppColors.dark)
 class AppColors {
   static Color main = const Color.fromRGBO(23, 77, 108, 1);
-  static Color mainDark = const Color.fromRGBO(17, 40, 46, 1);
   static Color accent = const Color.fromRGBO(132, 94, 194, 1);
 
   static Color cardBorder = const Color.fromRGBO(220, 220, 220, 0.5);
-  static Color neutralLight = const Color.fromRGBO(246, 245, 245, 1);
   static Color offwhite = const Color.fromRGBO(250, 244, 238, 1);
-  static Color neutralDark = const Color.fromRGBO(32, 38, 46, 1);
+  static Color neutralLight = const Color.fromRGBO(246, 245, 245, 1);
+  static Color neutralDark = const Color.fromRGBO(29, 29, 29, 1);
 
+  static Color green = const Color.fromRGBO(99, 230, 104, 1);
+  static Color red = const Color.fromRGBO(230, 99, 113, 1);
   static Color white = const Color.fromARGB(255, 255, 255, 255);
   static Color black = const Color.fromARGB(255, 0, 0, 0);
   static Color grey = const Color.fromARGB(255, 158, 158, 158);
