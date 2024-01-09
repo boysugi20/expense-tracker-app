f64ffbbe44431bff256b73b617a4bd542046e171
Boy Sugijakko <boysugijakko2000@gmail.com>
2024-01-09 19:23:01 +0700

    :zap: add icon

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,16 +1,32 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
-  static Future initialize(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async{
-    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.requestPermission();
-    var androidInit = const AndroidInitializationSettings('mipmap/ic_launcher');
+  static Future initialize(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestPermission();
+    var androidInit =
+        const AndroidInitializationSettings('mipmap/expense_tracker');
     var initializationSettingsDarwin = const DarwinInitializationSettings();
-    final InitializationSettings initializationSettings = InitializationSettings(android: androidInit,);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,);
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: androidInit,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
   }
 
-  static Future showNotification({var id=0, required String title, required String body, var payload, required FlutterLocalNotificationsPlugin fln}) async{
-    AndroidNotificationDetails androidNotificationDetails = const AndroidNotificationDetails(
+  static Future showNotification(
+      {var id = 0,
+      required String title,
+      required String body,
+      var payload,
+      required FlutterLocalNotificationsPlugin fln}) async {
+    AndroidNotificationDetails androidNotificationDetails =
+        const AndroidNotificationDetails(
       'default_notification_channel_id',
       'Default',
 
